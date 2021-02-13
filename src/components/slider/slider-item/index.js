74df7e124846219c275b5897639aad5897ed140a
raz0red <raz0red@gmail.com>
2021-02-13 00:21:49 -0800

    Slider label size tweaks

--- src/components/slider/slider-item/index.js
+++ src/components/slider/slider-item/index.js
@@ -6,11 +6,7 @@ const SliderItem = ({ app, width, selected, onClick }) => {
   return (
     <div className="slider-item" style={{ width: `${width}%` }} onClick={onClick}>
       <div className={'slider-item-container' + (selected ? ' slider-item-container__selected' : '')}>
-        <img
-          src={app.thumbnail} 
-          alt={app.title}
-          // src={'https://www.blockfort.com/wp-content/uploads/5814/3021/1922/NinjaGolf.png'}       
-        />
+        <img src={app.thumbnail}  alt={app.title}/>
         <div className={'slider-item-title' + (selected ? ' slider-item-title__selected' : '')}>{app.title}</div>
       </div>      
     </div>
