12ced6f5b48cf9f154012d6d196bde4465855410
raz0red <raz0red@gmail.com>
2021-04-16 17:10:29 -0700

    Added key to slider item images

--- src/components/slider/slider-item/index.js
+++ src/components/slider/slider-item/index.js
@@ -6,7 +6,7 @@ const SliderItem = ({ width, selected, onClick, title, thumbnailSrc, hide }) => 
   return (
     <div className="slider-item" style={{ width: `${width}%`, visibility: hide ? 'hidden' : 'visible' }} onClick={onClick}>
       <div className={'slider-item-container' + (selected ? ' slider-item-container__selected' : '')}>
-        <img src={thumbnailSrc} alt={title}/>
+        <img src={thumbnailSrc} key={thumbnailSrc} alt={title}/>
         <div className={'slider-item-title' + (selected ? ' slider-item-title__selected' : '')}>{title}</div>
       </div>      
     </div>
