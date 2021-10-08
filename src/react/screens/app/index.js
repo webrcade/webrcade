17a6a1e064d64e103de9bb56617d0b4ceebe4df6
raz0red <raz0red@gmail.com>
2021-10-08 11:58:26 -0700

    Added retry if frame fails to load in 10 seconds

--- src/react/screens/app/index.js
+++ src/react/screens/app/index.js
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
 import { AppRegistry } from '../../../apps';
-import { isDev } from '@webrcade/app-common'
+import { isDev, LOG } from '@webrcade/app-common'
 
 require("./style.scss");
 
 export default class AppScreen extends Component {
+
+  constructor() {
+    super();
+
+    this.frameRetryId = null;
+  }
+
+  MAX_LOAD_TIME = 10 * 1000;
+
+  clearRefreshTimout() {
+    const { frameRetryId } = this;
+
+    if (frameRetryId != null) {
+      clearTimeout(frameRetryId);
+      this.frameRetryId = null;
+    }
+  }
+
+
+  messageListener = e => {
+    if (e.data === 'appLoaded') {
+      LOG.info("app loaded message received.")
+      this.clearRefreshTimout();
+    }
+  }
+
+  componentDidMount() {    
+    const { frameRef } = this.props;
+    const { MAX_LOAD_TIME } = this;
+
+    window.addEventListener("message", this.messageListener);
+
+    this.frameRetryId = setTimeout(() => {
+      if (frameRef.current && !isDev()) {
+        LOG.info("attempting to reload frame, exceeded wait timeout: " + MAX_LOAD_TIME);
+        frameRef.current.contentDocument.location.reload();
+      }
+    }, MAX_LOAD_TIME);    
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("message", this.messageListener);
+
+    this.clearRefreshTimout();
+  }
+
   render() {
     const { app, frameRef } = this.props;
     const reg = AppRegistry.instance;
