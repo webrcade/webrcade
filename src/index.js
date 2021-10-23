706ea66858a8a03c469051c5cbabd69e94ee4afa
raz0red <raz0red@gmail.com>
2021-10-23 09:14:46 -0700

    Image preloading, slider thumbnail temp image

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from "react-dom";
 import '@webrcade/app-common/dist/index.css'
 import Webrcade from "./react/webrcade";
 
+import { 
+  preloadImages,
+  AddCircleBlackImage,
+  AddCircleWhiteImage,
+  CloudDownloadBlackImage,
+  CloudDownloadWhiteImage,
+  DeleteForeverBlackImage,
+  DeleteForeverWhiteImage,
+  PlayArrowBlackImage,
+  PlayArrowWhiteImage,
+  WebrcadeLogoDarkImage,
+  WebrcadeLogoLargeImage  
+} from '@webrcade/app-common'
+
+preloadImages([
+  WebrcadeLogoDarkImage,
+  WebrcadeLogoLargeImage,
+  AddCircleBlackImage,
+  AddCircleWhiteImage,
+  CloudDownloadBlackImage,
+  CloudDownloadWhiteImage,
+  DeleteForeverBlackImage,
+  DeleteForeverWhiteImage,
+  PlayArrowBlackImage,
+  PlayArrowWhiteImage
+]);
+
 ReactDOM.render(
   <React.StrictMode>
     <Webrcade />
