468bf46f42a575e2a3777feced50ffbc62924e00
Chris Smith <raz0red@gmail.com>
2023-03-19 07:57:27 -0700

    K7 (#127)
    * Neo Geo CD
    
    * quake images
    
    * Updated dist
    
    * Updated clone to include quake and neocd
    
    * Temporarily added cloning of k7 for 0.1.0
    
    * Fixed neocd clone
    
    * Updated prior to 0.1.0 release

--- dist-clone-deps.sh
+++ dist-clone-deps.sh
@@ -42,11 +42,15 @@ git clone https://github.com/webrcade/webrcade-app-retro-genplusgx.git ||
   { fail 'Unable to clone retro-genplusgx'; }
 git clone https://github.com/webrcade/webrcade-app-retro-pce-fast.git ||
   { fail 'Unable to clone retro-pce-fast'; }
-  git clone https://github.com/webrcade/webrcade-app-colem.git ||
+git clone https://github.com/webrcade/webrcade-app-colem.git ||
   { fail 'Unable to clone colem'; }
 git clone https://github.com/webrcade/webrcade-app-beetle-pcfx.git ||
   { fail 'Unable to clone beetle-pcfx'; }
 git clone https://github.com/webrcade/webrcade-app-retro-a5200.git ||
   { fail 'Unable to clone retro-a5200'; }
+git clone https://github.com/webrcade/webrcade-app-retro-neocd.git ||
+  { fail 'Unable to clone retro-neocd'; }
+git clone https://github.com/webrcade/webrcade-app-tyrquake.git ||
+  { fail 'Unable to clone quake'; }
 git clone https://github.com/webrcade/webrcade-app-standalone.git ||
   { fail 'Unable to clone standalone'; }
